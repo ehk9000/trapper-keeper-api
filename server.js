6f49245a08c390263d649fe267348cfbb688cd0d
Erik Konner <erikkonner@gmail.com>
2019-11-04 11:04:35 -0700

    Added port env variable and gitignore file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 
-app.set('port', 3001);
+app.set('port', process.env.PORT || 3001);
 
 app.listen(app.get('port'), () => {
-  console.log(`Trapper Keeper is running on http://localhost:${app.get('port')}.`)
+  console.log(`Trapper Keeper is running ⚡️ on http://localhost:${app.get('port')}.`)
 });
